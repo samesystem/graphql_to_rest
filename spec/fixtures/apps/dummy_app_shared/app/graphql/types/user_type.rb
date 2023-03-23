dab4b8252a23c56e67dcb8514df6c343f7d51ece
Povilas Jurčys <povilasjurcys@users.noreply.github.com>
2023-03-23 13:54:26 +0200

    Do not reference unused schemas (#11)

--- spec/fixtures/apps/dummy_app_shared/app/graphql/types/user_type.rb
+++ spec/fixtures/apps/dummy_app_shared/app/graphql/types/user_type.rb
@@ -8,12 +8,14 @@ module GraphqlToRest
       class UserType < GraphQL::Schema::Object
         require_relative './post_type'
         require_relative './gender_enum'
+        require_relative './unused_item_type'
 
         field :id, ID, 'User ID', null: false
         field :email, String, 'User email', null: false
         field :full_name, String, 'User full name', null: false
         field :gender, GenderEnum, 'User gender', null: true
         field :posts, [PostType, {null: false}], 'User posts', null: true
+        field :unusedItems, [UnusedItemType, {null: false}], 'This should not go to OpenAPI json', null: true
       end
     end
   end
